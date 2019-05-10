c93d8d9166978bb9099c8b37d0a107670ee06174
Camila <38863912+camilacvieira@users.noreply.github.com>
2019-05-10 10:51:08 -0300

    Add files via upload

--- Trabalho Ed2/main.cpp
+++ Trabalho Ed2/main.cpp
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 #include <fstream>
 #include <sstream>
-#define N 1000
+#include "Registro.h"
+#define N 5
 
 using namespace std;
 
@@ -179,6 +180,7 @@ void ManualSort(int *vet, int left, int right){
 }
 
 int Mediana(int *vet, int left, int right, int k){
+    /*
     int mid = (right+left)/2;
     if(vet[mid] < vet[left]){
         Troca(vet, mid, left);
@@ -189,8 +191,68 @@ int Mediana(int *vet, int left, int right, int k){
     if(vet[right] < vet[mid]){
         Troca(vet, right, mid);
     }
-    Troca(vet, mid, right-1);
+    Troca(vet, mid, right-1);*/
 
+    int *random = new int(k);
+
+    for(int i = 0; i < k; i++){
+        random[i] = rand()%N;
+    }
+    cout << "Valores random: " << endl;
+    for(int i = 0; i < k; i++){
+        cout << random[i] << "  ";
+    }
+    cout << endl << endl;
+    cout << "Valor do vetor: " << endl;
+    for(int i = 0; i < k; i++){
+        cout << vet[random[i]] << "  ";
+    }
+    cout << endl << endl;
+    QuickSort(random, 0, k-1);
+
+    for(int i = 0; i < k-1; i++){
+        for(int j = i+1; j < k; j++){
+            if(vet[random[i]] > vet[random[j]]){
+                cout << "Trocou" << endl;
+                Troca(vet, random[i], random[j]);
+            }
+        }
+    }
+    cout << "Valor do vetor: " << endl;
+    for(int i = 0; i < k; i++){
+        cout << vet[random[i]] << "  ";
+    }
+    cout << endl << endl;
+    cout << "Valor do random: " << endl;
+    for(int i = 0; i < k; i++){
+        cout << random[i] << "  ";
+    }
+    cout << endl << endl;
+    Troca(vet, random[k-2], N-1);
+    for(int i =0; i < N; i++){
+        cout << vet[i] << "  ";
+    }
+    cout << endl;
+    return vet[N-1];
+
+    /*int *random =  new int[k];
+    for(int i = 0; i < k; i++){
+        random[i] = rand()%tamanho;
+    }
+    ManualSort(random);
+    if(vet[random[2]] < vet[random[0]]){
+        Troca(vet, random[2], random[0]);
+    }
+    if(vet[random[2]] < vet[random[1]]){
+        Troca(vet, random[2], random[1]);
+    }
+    if(vet[random[1]] < vet[random[0]]){
+        Troca(vet, random[1], random[0]);
+    }
+    return random[2];*int *vetorTeste = new int(N);
+    for(int i = 0; i< N; i++){
+
+    }*/
 }
 
 void QuickSortMediana(int *vet, int left, int right, int k){
@@ -198,6 +260,7 @@ void QuickSortMediana(int *vet, int left, int right, int k){
     if(tamanho <= 3)
         ManualSort(vet, left, right);
     else{
+
         int mediana = Mediana(vet, left, right, k);
         int particao = Particao(vet, left, right, mediana);
 
@@ -236,46 +299,33 @@ void QuickSortInsertion(int *vet, int left, int right){
 
 int main()
 {
-//Cria vetor aleatório para teste
+
+
+
 /*
+	//Cria vetor aleatório para teste
 	srand(time(NULL));//Função recursiva QuickSort, pivo é o mais a direita
 	int *vet = new int[N];
 	for (int i = 0; i < N; i++)
 	{
 		vet[i] = rand() % 100;
 	}
-*/
 
-	srand(time(NULL));
-	int *vetor = new int[N];
-	int *vetorPosicoes = new int[N];
 
-	fstream myfile;
 
-	myfile.open("ratings.csv");
-	myfile.seekg(0, ios::end);
-	int bytes = myfile.tellg();
-	cout << "bytes: " << bytes << endl<<endl;
-	string str;
-
-	//imprimindo numeros random de 0 até bytes
-	for(int i = 0; i < 100; i++){
-        cout << rand()%bytes << endl;
+	for (int i = 0; i < N; i++)
+	{
+	cout << vet[i] << "  ";
 	}
 	cout << endl;
 
-	//leitura de arquivo
-	for(int i=0; i < N; i++){
-        int posicaoRandom = rand()%bytes;
-        vetorPosicoes[i] = posicaoRandom;
-        myfile.seekg(posicaoRandom, ios::beg);
-        getline(myfile, str);
-        getline(myfile, str, ',');
-        istringstream iss (str);
-        int aux;
-        iss >> aux;
-        vetor[i] = aux;
+	QuickSort(vet, 0, N-1);
+    cout << endl;
+	for (int i = 0; i < N; i++)
+	{
+	cout << vet[i] << "  ";
 	}
+*/
 
 //funções de ordenação e função de printVetor
 /*
@@ -298,6 +348,125 @@ int main()
 	cout << "CombSort:" << endl;
     CombSort(vetor,N);
     printVetor(vetor,N);
+*/
+
+
+	srand(time(NULL));
+	int *vetor = new int[N];
+
+	int *vetorPosicoes = new int[N];
+
+
+	fstream myfile;
+
+	myfile.open("ratings.csv");
+
+	/*
+	if(myfile.is_open()){
+        int userid;
+        int movieId;
+        string str;
+        getline(myfile, str);
+        getline(myfile, str);
+        getline(myfile, str);
+        cout << str << endl;
+        getline(myfile, str , ',');
+        cout << str << endl;
+        getline(myfile, str , ',');
+        cout << str << endl;
+
+	}
+*/
+
+
+	myfile.seekg(0, ios::end);
+	int bytes = myfile.tellg();
+	cout << "Bytes: " << bytes << endl;
+	string str;
+    string delimitador = ",";
+	for(int i=0; i < N; i++){
+        int contador = 0;
+        int userId ;
+        int movieId ;
+        float rating;
+        int timestamp;
+
+        int posicaoRandom = rand()%bytes;
+        vetorPosicoes[i] = posicaoRandom;
+        myfile.seekg(posicaoRandom, ios::beg);
+        getline(myfile, str);
+
+
+        getline(myfile, str);
+        cout << "String: " << str << endl;
+        int pos = 0;
+        istringstream iss;
+        string aux;
+        while((pos = str.find(delimitador)) != string::npos){
+            if(contador == 0){
+                aux = str.substr(0, str.find(delimitador));
+                iss.str(aux);
+                iss >> userId;
+                iss.clear();
+                str.erase(0, pos + delimitador.length());
+            }
+            else if(contador == 1){
+                aux = str.substr(0, str.find(delimitador));
+                iss.str(aux);
+                iss >> movieId;
+                iss.clear();
+                str.erase(0, pos + delimitador.length());
+            }
+            else if(contador == 2){
+                aux = str.substr(0, str.find(delimitador));
+                iss.str(aux);
+                iss >> rating;
+                iss.clear();
+                str.erase(0, pos + delimitador.length());
+            }
+            cout << "Aux: " << aux << endl;
+            contador++;
+        }
+        cout << "STR: " << str << endl;
+        iss.str(str);
+        iss >> timestamp;
+        cout << "UserId: " << userId << "  movieId: " << movieId << "  Rating: " << rating << "  Timestamp: " << timestamp << endl;
+
+
+        //vetor[i] = userId;
+
+	}
+	/*
+	for(int i=0; i < N; i++){
+        int posicaoRandom = rand()%bytes;
+        vetorPosicoes[i] = posicaoRandom;
+        //cout << posicaoRandom << endl;
+        myfile.seekg(posicaoRandom, ios::beg);
+        getline(myfile, str);
+        getline(myfile, str, ',');
+        istringstream iss (str);
+        int aux;
+        iss >> aux;
+        vetor[i] = aux;
+	}
+
+
+
+
+    for(int i =0; i < N; i++){
+        cout << vetor[i] << "  ";
+    }
+    cout << endl << endl;
+
+	if (myfile.is_open()) {
+        QuickSort(vetor, 0, N-1);
+        //InsertionSort(vetor, N);
+        //QuickSortMediana(vetor, 0, N-1, 3);
+        for(int i = 0; i < N; i++){
+            cout << vetor[i] << "  ";
+
+        }
+	}
 */
 
 
