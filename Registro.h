24091bf5b25c3cd8e186722d79e68bdcdafb9145
Camila <38863912+camilacvieira@users.noreply.github.com>
2019-05-13 15:42:18 -0300

    Add files via upload

---
+++ Registro.h
@@ -0,0 +1,29 @@
+#ifndef REGISTRO_H
+#define REGISTRO_H
+
+
+class Registro
+{
+public:
+    Registro(int userId, int movieId, float rating, int timestamp);
+    Registro();
+    ~Registro();
+
+    void setUserId(int id);
+    void setMovied(int id);
+    void setRating(float rating);
+    void setTimestamp(int timestamp);
+
+    int getUserId();
+
+    void ExibirRegistro();
+    void ExibirUserId();
+
+private:
+    int userId;
+    int movieId;
+    float rating;
+    int timestamp;
+};
+
+#endif // REGISTRO_H
